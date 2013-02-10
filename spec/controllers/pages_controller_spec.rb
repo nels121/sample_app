d060e5a021e89eaa49cfc4218554ba109212d0b9
Nick Herbold <herboldn@gmail.com>
2013-02-10 16:06:47 -0500

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,13 +7,29 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+  it "should have the right title" do
+    get 'home'
+    response.should be_success
+    end
   end
-
   describe "GET 'contact'" do
     it "returns http success" do
       get 'contact'
       response.should be_success
     end
+  it "should have the right title" do
+    get 'contact'
+    response.should be_success
+    end
   end
-
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  it "should have the right title" do
+    get 'about'
+    response.should be_success
+    end
+  end
 end
